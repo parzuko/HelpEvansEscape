e3607781452796f08501b1fe903b1809c0e36238
parzuko <jivansh17@gmail.com>
2020-08-24 10:33:45 +0530

    Set Up Basic Files

---
+++ help_evans_escape/lib/configs/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  //Standard Colors everywhere
+  static const Color scaffold = Color(0xffFFFFFF);
+  static const Color textColor = Color(0xff000000);
+  static const Color iconBack = Color(0xffFBD07A);
+  static const Color brightText = Color(0xffF44292);
+}
