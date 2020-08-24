45143214f223321d58effbb45b1beafa2589106f
parzuko <jivansh17@gmail.com>
2020-08-24 10:54:59 +0530

    Set Up Basic App & Colors

--- help_evans_escape/lib/main.dart
+++ help_evans_escape/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'HelpEvansEscape',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Palette.iconBack,
+        primarySwatch: Colors.lime,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         scaffoldBackgroundColor: Palette.scaffold,
       ),
--- help_evans_escape/lib/screens/home_screen.dart
+++ help_evans_escape/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:help_evans_escape/configs/palette.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Palette.scaffold,
+        elevation: 0.0,
+        title: Text(
+          "Help Evans Escape !",
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+        child: Container(
+          height: 800,
+          color: Palette.iconBack,
+        ),
+      ),
+    );
   }
 }
